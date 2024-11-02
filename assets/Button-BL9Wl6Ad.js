daa8da477e3a31afff2b833d425b4a9c87c5b99a
RidaKhaoua <RidaKhaoua@users.noreply.github.com>
2024-11-02 16:27:15 +0000

    deploy: 1be2fa2839c7ff87eb9ce8df711c876b98b20406

---
+++ assets/Button-BL9Wl6Ad.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BZjRF0JQ.js";import"./singInShema-BTPDx30G.js";const x=({children:t,style:o,type:r="button",disabled:s=!1,className:n="",...u})=>e.jsx("button",{type:r,...u,style:o,disabled:s,className:n,children:t});export{x as B};
