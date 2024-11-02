630d220a11834f4f753c94727b6834bcf0f44bb1
RidaKhaoua <RidaKhaoua@users.noreply.github.com>
2024-11-02 16:48:30 +0000

    deploy: 09fbc857f98a3c736e6b986f14d67a7094a3d9b9

--- assets/Loading-Csqn5nEr.js
+++
@@ -1,1 +0,0 @@
-import{j as n,p as s}from"./index-BZjRF0JQ.js";function o({status:t,error:i,children:e}){return n.jsx("div",{children:t==="pending"?n.jsx(s,{type:"loading"}):i?n.jsx("p",{children:i}):e})}export{o as L};
